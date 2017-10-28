2c64279af2b3b74deabb726977473ffd40088f7e
Naoki Ishimura <mirontest1460@gmail.com>
2017-10-29 08:43:45 +0900

    Fix design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返す
   def full_title(page_title = '')
-    base_title = "Simple Graph"
+    base_title = "Simple Data Graph"
     if page_title.empty?
       base_title
     else
